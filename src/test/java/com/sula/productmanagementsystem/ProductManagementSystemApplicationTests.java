82d4cf36f5fcb0d8baa66b465684e35f248b8890
sulakkhanaid <sulakkhana@deeplab.lk>
2021-05-24 19:28:19 +0530

    my first commit

---
+++ src/test/java/com/sula/productmanagementsystem/ProductManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sula.productmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
